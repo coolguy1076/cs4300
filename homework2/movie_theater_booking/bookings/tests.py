026f7104352151d9a8f6eeb975456a0254e21490
coolguy1076 <mtbieker@comcast.net>
2026-03-06 04:43:28 +0000

    fixed several templating issues in movie_list.html that resulted from copying directly from the assignment guidelines in homework2

--- homework2/movie_theater_booking/bookings/tests.py
+++ homework2/movie_theater_booking/bookings/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Movie
 
 # Create your tests here.
+
+class MovieTest(TestCase):
+
+    def test_movie_creation(self):
+
+        movie = Movie.objects.create(
+            title="Test Movie",
+            description="Test Description",
+            release_date="2025-01-01",
+            duration=120
+        )
+
+        self.assertEqual(movie.title, "Test Movie")
